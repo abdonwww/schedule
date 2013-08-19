bbb24929efc106f745471d6629cf7f4c4d09648e
abdonwww <abdonwww@gmail.com>
2013-08-20 07:02:01 +0900

    schedule

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,94 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('HomeController', ['$scope', '$window', '$location', '$resource', '$http', '$filter', function($scope, $window, $location, $resource, $http, $filter) {
+  	var dates = [];
+  	for(var i=0; i<=90; i++) {
+      var time = new Date().getTime() + 1000 * 60 * 60 * 24 * i;
+
+  		dates.push({
+  			date: $filter('date')(time, 'dd'),
+  			day: $filter('date')(time, 'EEE')
+  		})
+  	}
+  	$scope.dates = dates;
+
+    $scope.rowNum = -1;
+    $scope.colNum = -1;
+
+    $scope.rowRef = function(num){
+      $scope.rowNum = num;
+    };
+    $scope.colRef = function(num){
+      $scope.colNum = num;
+    };
+    $scope.tableReset = function(){
+      $scope.rowNum = -1;
+      $scope.colNum = -1;
+    };
+
+
+  	$scope.$window = $window;
+
+    $scope.initHome = function(){
+      $scope.rowHeight = $window.innerHeight - 100;
+    }
+
+    // $resource('data/members.json').get(function(data) {
+    //   $scope.members = data;
+    // });
+
+  	$scope.members = [
+  		{ name: 'Naoki Ueno', schedule: dates },
+      { name: 'Naoki Ueno', schedule: dates },
+      { name: 'Naoki Ueno', schedule: dates },
+      { name: 'Naoki Ueno', schedule: dates },
+      { name: 'Naoki Ueno', schedule: dates },
+      { name: 'Naoki Ueno', schedule: dates },
+  		{ name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Ryosuke Tsuji', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+      { name: 'Yosuke Sato', schedule: dates },
+  		{ name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+      { name: 'Momo Taniwaki', schedule: dates },
+  		{ name: 'Poore Amanda', schedule: dates },
+      { name: 'Poore Amanda', schedule: dates },
+      { name: 'Poore Amanda', schedule: dates },
+      { name: 'Poore Amanda', schedule: dates },
+      { name: 'Poore Amanda', schedule: dates },
+      { name: 'Poore Amanda', schedule: dates },
+  		{ name: 'Taketo Aihara', schedule: dates },
+      { name: 'Taketo Aihara', schedule: dates },
+      { name: 'Taketo Aihara', schedule: dates },
+      { name: 'Taketo Aihara', schedule: dates },
+      { name: 'Taketo Aihara', schedule: dates }
+  	];
+
+
+
+  }])
+  .controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,42 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }])
+  .directive('dateCell', [function factory(injectables) {
+    var directiveDefinitionObject = {
+      restrict: 'E',
+      replace: true,
+      template: '<div class="test">HelloWOrld</div>'
+    };
+    return directiveDefinitionObject;
+  }])
+  .directive('ngScroll', ['$window', function factory($window) {
+    var top = 0,
+        left = 0;
+
+    return function(scope, element, attr) {
+      var header = angular.element('#x-axis').get(0);
+      var body = angular.element('#rows').get(0);
+      var cells = angular.element('#cells').get(0);
+
+      element.bind('mousewheel', function(e, delta, deltaX, deltaY) {
+        left += deltaX;
+        top -= deltaY;
+
+        left = left > 0 ? left : 0;
+        top = top > 0 ? top : 0;
+
+        scope.left = left;
+        scope.top = top;
+
+        header.scrollLeft = left;
+        cells.scrollLeft = left;
+        body.scrollTop = top;
+
+        e.preventDefault();
+        
+      });
+    };
   }]);
+
+
